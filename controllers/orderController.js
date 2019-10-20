05d4edc152d3a24d82c6a2681b15b33c3b9022a2
xremix <tonidhoffmann@gmail.com>
2019-10-20 10:29:29 +0200

    clean up of index.html

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,5 @@
-const orderRepository = require('../repositories/orderRepository');
+// const orderRepository = require('../repositories/orderRepository');
+const orderRepository = require('../repositories/orderRepositoryMock');
 
 exports.orderByCustomerId = function(req, res) {
   orderRepository.orderByCustomer(req.params.customerId, function(orders) {
